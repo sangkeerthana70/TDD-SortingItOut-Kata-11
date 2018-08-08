3b5e834627b1842134c31166c01c193cfd93d65b
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-07 22:20:26 -0400

    Three test cases passed

--- SortingBalls/Rack.cs
+++ SortingBalls/Rack.cs
@@ -9,16 +9,43 @@ namespace SortingBalls
 {
     class Rack
     {
-        public int[] balls; 
-
+        //property
+        public List<int> balls;
+        
+        //constructor
         public Rack()
         {
 
-            this.balls = new int[] {};
+            this.balls = new List<int>() {};
         }
-       
-       
+
+        //method to add balls and sort
+        public void AddBalls(int ball)
+        {
+            //adds the next ball draw to the existting list of balls
+            balls.Add(ball);
+            balls.Sort();
+        }
+
+        //method to print values from the list
+        public void PrintListValues()
+        {
+            foreach(int ball in balls)
+                Console.WriteLine("Balls in the draw: " + ball);
+
+        } 
+            
     }
+
 }
+         
+
+
+
+
+       
+       
+    
+
 
 
--- SortingBalls/Test.cs
+++ SortingBalls/Test.cs
@@ -13,8 +13,29 @@ namespace SortingBalls
         public static void Test_EmptyNumbers()
         {
             Rack testRack = new Rack();
-            Assert.AreEqual(new int[] {}, testRack.balls);
+            Assert.AreEqual(new List<int>() { }, testRack.balls);
+            Console.WriteLine("Test Case 1 executed");
+            testRack.PrintListValues();
+
+
+            //second test case
+            Console.WriteLine("Test Case 2");
+            testRack.AddBalls(20);
+            Assert.AreEqual(new List<int>() { 20 }, testRack.balls);           
+            testRack.PrintListValues();
+
+
+            //third test case
+            Console.WriteLine("Test Case 3");
+            testRack.AddBalls(10);
+            testRack.BallSort();
+            testRack.PrintListValues();
+            Assert.AreEqual(new List<int>() { 10, 20 }, testRack.balls);
+            
+            
             
         }
+
+        
     }
 }
