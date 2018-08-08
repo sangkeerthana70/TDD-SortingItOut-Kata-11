5907ff356b5d2f57494fa30669825d4cfdad602b
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-07 21:00:38 -0400

    SortingBalls Kata Initial test case

---
+++ SortingBalls/Rack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingBalls
+{
+    class Rack
+    {
+        public int[] balls; 
+
+        public Rack()
+        {
+
+            this.balls = new int[] {};
+        }
+       
+       
+    }
+}
+
+
---
+++ SortingBalls/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SortingBalls
+{
+    class Test
+    {
+        [Test]
+        public static void Test_EmptyNumbers()
+        {
+            Rack testRack = new Rack();
+            Assert.AreEqual(new int[] {}, testRack.balls);
+            
+        }
+    }
+}
